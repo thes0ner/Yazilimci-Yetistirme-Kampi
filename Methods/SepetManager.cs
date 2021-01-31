f310f882a2d4c93ea86bf64bf8ad57cf2be4fc99
Soner <thes0ner@outlook.com>
2021-01-31 22:50:47 +0100

    Add project files.

---
+++ Methods/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine(urun.Adi + " Sepetinize Eklendi");
+        }
+
+        public void yeniEkle(string urunAdi, string aciklama, double fiyat)
+        {
+            Console.WriteLine(urunAdi + " Sepetinize Eklendi");
+        }
+    }
+}
