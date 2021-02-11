008dbaf8a8f15b4aec0c54ef12fff05dff1555d5
Soner <thes0ner@outlook.com>
2021-02-11 15:47:58 +0100

    Eklendi

---
+++ GenericsIntro/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyDictionary<int32, T>
+    {
+        //Fields
+        int[] _value;
+        int[] _tempValue;
+
+        T[] _key;
+        T[] _tempKey;
+
+        //Constructor
+        public MyDictionary()
+        {
+            _value = new int[0];
+            _key = new T[0];
+        }
+
+        //Method
+        public void Add(int value, T key)
+        {
+            _tempValue = _value;
+            _tempKey = _key;
+
+            _value = new int[_value.Length + 1];
+            _key = new T[_key.Length + 1];
+
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+                _key[i] = _tempKey[i];
+
+            }
+            _value[_value.Length - 1] = value;
+            _key[_key.Length - 1] = key;
+
+        }
+        public int Count { get { return _value.Length; } }
+
+        public void Show()
+        {
+            for (int i = 0; i < _value.Length; i++)
+            {
+                Console.WriteLine("ID: " + _value[i] + " Name: " + _key[i]);
+
+            }
+        }
+    }
+}
--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -6,16 +6,29 @@ namespace GenericsIntro
     {
         static void Main(string[] args)
         {
-            MyList<string> isimler = new MyList<string>();
-            isimler.Add("Soner");
-            Console.WriteLine(isimler.Length);
-            isimler.Add("Engin");
-            Console.WriteLine(isimler.Length);
+            //MyList<string> isimler = new MyList<string>();
+            //isimler.Add("Soner");
+            //Console.WriteLine(isimler.Length);
+            //isimler.Add("Engin");
+            //Console.WriteLine(isimler.Length);
 
-            foreach (var isim in isimler.Items)
-            {
-                Console.WriteLine(isim);
-            }
+            //foreach (var isim in isimler.Items)
+            //{
+            //    Console.WriteLine(isim);
+            //}
+
+
+            //Dictionary
+
+            MyDictionary<int, string> myDictionary1 = new MyDictionary<int, string>();
+            myDictionary1.Add(0, "Engin");
+            myDictionary1.Add(1, "Soner");
+            myDictionary1.Add(2, "Deniz");
+            myDictionary1.Add(3, "Ahmet");
+            myDictionary1.Add(4, "Cengiz");
+            Console.WriteLine("Entered Value: " + myDictionary1.Count);
+            myDictionary1.Show();
+
 
 
         }
